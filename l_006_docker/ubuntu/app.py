61c064bac9d68f0e52c3a1bde803012641e6b8ff
shpak <serg.shp@gmail.com>
2021-06-15 16:14:19 +0300

    Docker002_Ubuntu

---
+++ l_006_docker/ubuntu/app.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import executor
+from config import admin_id
+from load_all import bot, create_db
+
+
+async def on_shutdown(dp):
+    await bot.close()
+
+
+async def on_startup(dp):
+    await asyncio.sleep(10)
+    await create_db()
+    await bot.send_message(admin_id, "Я запущен!")
+
+
+if __name__ == '__main__':
+    from handlers import dp
+
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
